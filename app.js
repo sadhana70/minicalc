9799c69a1b9dadb726f268fe92a4424d45e3880a
Sadhana Panthi <panthisadhana123@gmail.com>
2023-10-05 20:41:06 +0545

    js complete

--- app.js
+++ app.js
@@ -11,3 +11,19 @@
                 screen.value += value;
             })
     });
+
+    equal.addEventListener('click', function(e){
+        if(screen.value === ''){
+            screen.value = "";
+        }else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
+    })
+
+    clear.addEventListener('click', function(e){
+        screen.value = "";
+    })
+
+})();
+
